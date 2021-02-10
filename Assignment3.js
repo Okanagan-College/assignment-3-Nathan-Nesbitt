c165665836400cf81cd304fbcb4feb5f784c7c00
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-09 18:33:27 -0800

    Added some basic JS

--- Assignment3.js
+++ Assignment3.js
@@ -33,7 +33,44 @@ class Calculator {
     }
   
     calculate(){
-      
-     
+        // Anytime someone clicks, run the code inside
+        document.addEventListener("click", (event) => {
+            const { target } = event;
+            
+            // If the button clicked is of class "enter" run this code
+            if(target.classList.contains("enter")) {
+                console.log("ENTER WAS PRESSED!")
+            }
+
+            // Else if you clicked a button with class "number"
+            else if(target.classList.contains("number")) {
+                console.log("NUMBER WAS PRESSED!")
+            }
+
+            // Else if you clicked a button with class "operator"
+            else if(target.classList.contains("operator")) {
+                console.log("OPERATOR WAS PRESSED!")
+            }
+            
+            // Else if you clicked a button with class "decimal"
+            else if(target.classList.contains("decimal")) {
+                console.log("DECIMAL WAS PRESSED!")
+            }
+            // Else if you clicked a button with class "clear"
+            else if(target.classList.contains("clear")) {
+                console.log("CLEAR WAS PRESSED!")
+            }
+        })
     }
 }
+
+// Handle the document loading and generating the code for the calculator //
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Creating an instance of an Calculator object //
+    var calculator = new Calculator();
+
+    // Run the main calculator function! //
+    calculator.calculate();
+
+});
